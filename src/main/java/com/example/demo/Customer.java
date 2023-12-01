9f37c7f88c6d5b1ddb0d564dec6370dbc688dea8
sumitkumar <sumitkumar1202@gmail.com>
2023-12-01 16:07:13 +0530

    commit2

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,10 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @Min(value = 0, message = "value should be 0 or greater")
+    @Max(value = 10, message = "value should be 10 or lesser")
+    private String freePasses;
 
     public Customer() {
     }
@@ -27,6 +33,14 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(String freePasses) {
+        this.freePasses = freePasses;
     }
 
 }
--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -11,6 +14,13 @@ import jakarta.validation.Valid;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
     @GetMapping("/customerForm")
     public String customerForm(Model model) {
         model.addAttribute("customer", new Customer());
@@ -19,6 +29,7 @@ public class CustomerController {
 
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute Customer customer, BindingResult bindingResult) {
+        System.out.println("My Log:: Last Name: |" + customer.getLastName() + "|");
         if (bindingResult.hasErrors()) {
             return "customerForm";
         } else {
